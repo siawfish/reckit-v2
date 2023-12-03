72c6631ef0f042d49da9ed7ab881d760a07363e3
Gerald <mcamanor@gmail.com>
2023-12-03 10:02:13 +0000

    Included prod error logs

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import Bugsnag from '@bugsnag/expo';
+
+Bugsnag.start();
 import 'react-native-gesture-handler'
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
@@ -5,14 +8,36 @@ import StackNavigation from './navigation/StackNavigation';
 import {PersistedStore, Store} from "./redux/store";
 import {PersistGate} from 'redux-persist/integration/react';
 import {Provider} from 'react-redux'
+const ErrorBoundary = Bugsnag.getPlugin('react').createErrorBoundary(React)
+
+const ErrorView = () =>
+  <View
+    style={{
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+  >
+    <Text
+      style={{
+        fontSize: 24,
+        fontWeight: 'bold',
+        fontFamily: 'Open-Sans',
+      }}
+    >
+      Sorry, something terribly bad happened.
+    </Text>
+  </View>
 
 export default function App() {
   return (
-    <Provider store={Store}>
-      <PersistGate loading={null} persistor={PersistedStore}>
-        <StatusBar style="auto" />
-        <StackNavigation />
-      </PersistGate>
-    </Provider>
+    <ErrorBoundary FallbackComponent={ErrorView}>
+      <Provider store={Store}>
+        <PersistGate loading={null} persistor={PersistedStore}>
+          <StatusBar style="auto" />
+          <StackNavigation />
+        </PersistGate>
+      </Provider>
+    </ErrorBoundary>
   );
 }
